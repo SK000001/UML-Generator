8a78e521fa7bf76ced5d8f11d9c8941e593ff6ac
SK000001 <31275278+SK000001@users.noreply.github.com>
2025-06-24 01:44:06 +1000

    Modularity added to UmlGenerator class.
    1) Created class UmlGenerator.
    2) Can generate uml for particular relationship or all relationships together.

--- uml_generator.py
+++ uml_generator.py
@@ -1,41 +1,58 @@
-from class_object import ClassObject
+from class_object import ClassObject, Relation, RelationType
 from graphviz import Digraph
 
-def generate_uml(class_obj: ClassObject):    
-    dot = Digraph()
-    
-    cls_name = class_obj.class_name
-    label = f"""<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0"><TR><TD BGCOLOR="lightgray"><B>{cls_name}</B></TD></TR><TR><TD ALIGN="LEFT">"""
-
-    for var_name, var_type in class_obj.class_variables:
-        label += f'+ {var_name} : {var_type}<BR ALIGN="LEFT"/>\n'
-    
-    label += f"""</TD></TR><TR><TD ALIGN="LEFT">"""
-    for mod, func_name, return_type in class_obj.class_functions:
-        label += f'{mod} {func_name} : {return_type}<BR ALIGN="LEFT"/>\n'
-    
-    label += "</TD></TR></TABLE>>" 
-    
-    dot.node(cls_name, label=label, shape="plaintext")
-    
-    for inher in class_obj.class_relationships.inheritances:
-        dot.node(inher, shape="box")
-        dot.edge(inher, cls_name, arrowhead="onormal")
+class UmlGenerator:
+    # Generates uml for a single class currently
+    def __init__(self, class_obj: ClassObject) -> None:
+        self.class_obj = class_obj
+        self.cls_name = class_obj.class_name
         
-    for asso in class_obj.class_relationships.associations:
-        dot.node(asso, shape="box")
-        dot.edge(cls_name, asso, arrowhead="vee")
+        self.dot = Digraph()
+        
+        self.label = f"""<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0"><TR><TD BGCOLOR="lightgray"><B>{self.cls_name}</B></TD></TR><TR><TD ALIGN="LEFT">"""
+        
+    def render_graph(self):
+        self.dot.render("uml_diagram", format="png", view=True)
+        
+    def generate_relation_node(self, relation: Relation):
+            self.dot.node(relation.source_class, shape="box")
+            
+            arrowhead, style = relation.get_arrow_style()
+            self.dot.edge(relation.source_class, relation.target_class, arrowhead=arrowhead, style=style)
+        
+    def generate_relationship(self, relation_type: RelationType):
+        for relation in self.class_obj.class_relationships:
+            if relation.relation_type == relation_type:
+                self.generate_relation_node(relation)
     
-    for comp in class_obj.class_relationships.compositions:
-        dot.node(comp, shape="box")
-        dot.edge(cls_name, comp, arrowhead="diamond")
+    def generate_inheritances(self):
+        self.generate_relationship(RelationType.INHERITANCE)
         
-    for aggr in class_obj.class_relationships.aggregations:
-        dot.node(aggr, shape="box")
-        dot.edge(cls_name, aggr, arrowhead="odiamond")
+    def generate_associations(self):
+        self.generate_relationship(RelationType.ASSOCIATION)
         
-    for dep in class_obj.class_relationships.dependencies:
-        dot.node(dep, shape="box")
-        dot.edge(cls_name, dep, style="dashed")
+    def generate_compositions(self):
+        self.generate_relationship(RelationType.COMPOSITION)
         
-    dot.render("class_diagram", format="png")
+    def generate_aggregations(self):
+        self.generate_relationship(RelationType.AGGREGATION)
+    
+    def generate_dependencies(self):
+        self.generate_relationship(RelationType.DEPENDENCY)
+        
+    def add_class_box(self):
+        for var_name, var_type in self.class_obj.class_variables:
+            self.label += f'+ {var_name} : {var_type}<BR ALIGN="LEFT"/>\n'
+        
+        self.label += f"""</TD></TR><TR><TD ALIGN="LEFT">"""
+        for access, signature, ret_type in self.class_obj.class_functions:
+            self.label += f'{access.value} {signature} : {ret_type}<BR ALIGN="LEFT"/>\n'
+            
+        self.label += "</TD></TR></TABLE>>"
+        self.dot.node(self.cls_name, label=self.label, shape="plaintext")
+    
+    def generate_all_relationships(self):
+        for relation in self.class_obj.class_relationships:
+            arrowhead, style = relation.get_arrow_style()
+            self.dot.edge(relation.source_class, relation.target_class, arrowhead=arrowhead, style=style)
+        
