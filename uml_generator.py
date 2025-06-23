98bd36f7ccda2d0f06039dc543acc5148ce74704
SK000001 <31275278+SK000001@users.noreply.github.com>
2025-06-24 04:30:52 +1000

    Removed code redundancy and added modularity.

--- uml_generator.py
+++ uml_generator.py
@@ -11,35 +11,6 @@ class UmlGenerator:
         
         self.label = f"""<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0"><TR><TD BGCOLOR="lightgray"><B>{self.cls_name}</B></TD></TR><TR><TD ALIGN="LEFT">"""
         
-    def render_graph(self):
-        self.dot.render("uml_diagram", format="png", view=True)
-        
-    def generate_relation_node(self, relation: Relation):
-            self.dot.node(relation.source_class, shape="box")
-            
-            arrowhead, style = relation.get_arrow_style()
-            self.dot.edge(relation.source_class, relation.target_class, arrowhead=arrowhead, style=style)
-        
-    def generate_relationship(self, relation_type: RelationType):
-        for relation in self.class_obj.class_relationships:
-            if relation.relation_type == relation_type:
-                self.generate_relation_node(relation)
-    
-    def generate_inheritances(self):
-        self.generate_relationship(RelationType.INHERITANCE)
-        
-    def generate_associations(self):
-        self.generate_relationship(RelationType.ASSOCIATION)
-        
-    def generate_compositions(self):
-        self.generate_relationship(RelationType.COMPOSITION)
-        
-    def generate_aggregations(self):
-        self.generate_relationship(RelationType.AGGREGATION)
-    
-    def generate_dependencies(self):
-        self.generate_relationship(RelationType.DEPENDENCY)
-        
     def add_class_box(self):
         for var_name, var_type in self.class_obj.class_variables:
             self.label += f'+ {var_name} : {var_type}<BR ALIGN="LEFT"/>\n'
@@ -50,9 +21,44 @@ class UmlGenerator:
             
         self.label += "</TD></TR></TABLE>>"
         self.dot.node(self.cls_name, label=self.label, shape="plaintext")
-    
-    def generate_all_relationships(self):
+        
+    def __generate_relation_node__(self, relation: Relation):
+        self.dot.node(relation.target_class, shape="box")
+        
+        arrowhead, style = relation.get_arrow_style()
+            
+        self.dot.edge(relation.source_class, relation.target_class, arrowhead=arrowhead, style=style)
+        
+    def __generate_relationship__(self, relation_type: RelationType | None = None):
+        """
+        Generate relationships of a specific type or all relationships if None is passed.
+        
+        Args:
+            relation_type: The type of relationship to generate, or None for all types.
+        """
+        
         for relation in self.class_obj.class_relationships:
-            arrowhead, style = relation.get_arrow_style()
-            self.dot.edge(relation.source_class, relation.target_class, arrowhead=arrowhead, style=style)
+            if relation_type is None or relation.relation_type == relation_type:
+                self.__generate_relation_node__(relation)
+                
+    def generate_all_relationships(self):
+        self.__generate_relationship__()
+    
+    def generate_inheritances(self):
+        self.__generate_relationship__(RelationType.INHERITANCE)
+        
+    def generate_associations(self):
+        self.__generate_relationship__(RelationType.ASSOCIATION)
+        
+    def generate_compositions(self):
+        self.__generate_relationship__(RelationType.COMPOSITION)
+        
+    def generate_aggregations(self):
+        self.__generate_relationship__(RelationType.AGGREGATION)
+    
+    def generate_dependencies(self):
+        self.__generate_relationship__(RelationType.DEPENDENCY)
+        
+    def render_graph(self):
+        self.dot.render("uml_diagram", format="png", view=True)
         
